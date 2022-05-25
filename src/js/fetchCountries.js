2dbb364e008da3b23f178e931387eaa76a0ecc64
Oleksii Borshch <sergeevi44@gmail.com>
2022-05-25 14:27:18 +0300

    add async-await branch

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -5,34 +5,31 @@ import countryInfoTpl from '../templates/country-info.hbs';
 import { clearMarkup } from '../index';
 const refs = links();
 
-export function fetchCountries(name) {
-  fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(response => {
-      if (response.length > 10) {
-        clearMarkup();
-        return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-      }
-      if ((response.length >= 2) & (response.length <= 10)) {
-        clearMarkup();
-        refs.countryList.insertAdjacentHTML('beforeend', countriesListTpl(response));
-        return;
-      }
-      if (response.length === 1) {
-        clearMarkup();
-        refs.countryInfo.insertAdjacentHTML('beforeend', countryInfoTpl(response));
-      }
-    })
-    .catch(error => {
+export async function fetchCountries(name) {
+  try {
+    const result = await fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
+    );
+    if (!result.ok) {
+      throw new Error(result.status);
+    }
+    const response = await result.json();
+    if (response.length > 10) {
       clearMarkup();
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-      return error;
-    });
+      return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    }
+    if ((response.length >= 2) & (response.length <= 10)) {
+      clearMarkup();
+      refs.countryList.insertAdjacentHTML('beforeend', countriesListTpl(response));
+      return;
+    }
+    if (response.length === 1) {
+      clearMarkup();
+      refs.countryInfo.insertAdjacentHTML('beforeend', countryInfoTpl(response));
+    }
+  } catch (error) {
+    clearMarkup();
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+    return error;
+  }
 }
